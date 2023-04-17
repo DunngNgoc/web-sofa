482b49057ddb22ac2ec8e884eef703545cfc0f3c
DunngNgoc <22070023@vnu.edu.vn>
2023-04-17 13:57:04 +0700

    add-project

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.banner').slick({
+    infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+$('.list-feedback').slick({
+    infinite: true,
+  slidesToShow: 1,
+  speed:800,
+  slidesToScroll: 1,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+$('.slide-product').slick({
+    infinite: true,
+  slidesToShow: 3,
+  speed:800,
+  slidesToScroll: 1,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
